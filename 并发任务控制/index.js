ea07dda90db41a2313569ad7145891623fdbbfe7
高健 <136210555@qq.com>
2023-03-11 03:19:41 +0800

    新增发任务并控制

--- 并发任务控制/index.js
+++ 并发任务控制/index.js
@@ -43,9 +43,8 @@ function addTask(time, name) {
     superTask.add(() => timeout(time)).then(() => console.log(`任务${name}完成`));
 }
 
-
-addTask(10000, 1);
-addTask(5000, 2);
-addTask(3000, 3);
-addTask(4000, 4);
-addTask(5000, 5);
+addTask(10000, 1); // 10000ms后输出：任务1完成
+addTask(5000, 2);  // 5000ms后输出：任务2完成
+addTask(3000, 3);  // 8000ms后输出：任务3完成
+addTask(4000, 4);  // 12000ms后输出：任务4完成
+addTask(5000, 5);  // 15000ms后输出：任务5完成
