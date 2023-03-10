012d8269762506a5c969390ed5f0643260963aa0
高健 <136210555@qq.com>
2023-03-11 03:15:49 +0800

    新增发任务并控制

---
+++ 并发任务控制/index.js
@@ -0,0 +1,51 @@
+class SuperTask {
+    constructor(count = 2) {
+        this.runningCount = 0;
+        this.parallelCount = count; // 一次请求时的并发数量
+        this.tasks = [];
+    }
+
+    add(task) {
+        return new Promise((resolve, reject) => {
+            this.tasks.push({
+                task,
+                resolve,
+                resolve,
+            });
+            this.runTask();
+        })
+    }
+
+    // 依次运行任务列表中的所有任务
+    runTask() {
+        while(this.runningCount < this.parallelCount && this.tasks.length > 0) {
+            const { task, resolve, reject } = this.tasks.shift();
+            this.runningCount ++;
+            // 此处并不严谨哈，应该需要考虑边界值，就是非promise对象的情况，这里暂时不考虑
+            task().then(resolve, reject).finally(() => {
+                this.runningCount --;
+                this.runTask();
+            })
+        }
+    }
+}
+
+
+function timeout(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => { resolve() }, time);
+    })
+}
+
+const superTask = new SuperTask();
+
+function addTask(time, name) {
+    superTask.add(() => timeout(time)).then(() => console.log(`任务${name}完成`));
+}
+
+
+addTask(10000, 1);
+addTask(5000, 2);
+addTask(3000, 3);
+addTask(4000, 4);
+addTask(5000, 5);
